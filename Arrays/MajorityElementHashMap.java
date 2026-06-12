f9f73d3e71f8bb312acc38d367935b2bf326481a
Pranav Adhav <142861984+Adhavpranav@users.noreply.github.com>
2026-06-12 10:02:31 +0530

    Add MajorityElementHashMap class implementation
    Implement a solution to find the majority element using a HashMap.

---
+++ Arrays/MajorityElementHashMap.java
@@ -0,0 +1,15 @@
+package array;
+
+import java.util.*;
+public class MajorityElementHashMap {
+    public int solution(int arr[]){
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+            if(map.get(arr[i])>(arr.length/2)){
+                return arr[i];
+            }
+        }
+        return -1;
+    }   
+}
