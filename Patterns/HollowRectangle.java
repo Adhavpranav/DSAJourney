ac434713a5d22ee1fa68dc6c718a7744dc8530f0
Pranav Adhav <142861984+Adhavpranav@users.noreply.github.com>
2026-07-31 19:23:13 +0530

    Fix formatting by adding newline at end of file

---
+++ Patterns/HollowRectangle.java
@@ -0,0 +1,11 @@
+public void solution(int row,int col){
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(j==0 || j==col-1||i==0 ||i==row-1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+}  
