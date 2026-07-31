45daed716c1c41e85e99a6b9e5696825de075432
Pranav Adhav <142861984+Adhavpranav@users.noreply.github.com>
2026-07-31 19:47:53 +0530

    Create InvertedHalfPyramid.java

---
+++ Patterns/InvertedHalfPyramid.java
@@ -0,0 +1,8 @@
+public void solution(int num){
+        for(int i=num;i>0;i--){
+            for(int j=i;j>0;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+}  
