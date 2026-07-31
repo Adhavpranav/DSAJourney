4ddec017c0fd0c37b57d9f5173014b94e12881b6
Pranav Adhav <142861984+Adhavpranav@users.noreply.github.com>
2026-07-31 19:39:24 +0530

    Add HalfPyramid class with solution method

---
+++ Patterns/HalfPyramid.java
@@ -0,0 +1,8 @@
+ public void solution(int num){
+        for(int i=0;i<num;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+}  
